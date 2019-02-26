9b8fe2acb3bd94370221e673ae3c16ffe1052d82
Martins Akermanis <m.akermanis@gmail.com>
2019-02-26 16:46:24 +0200

    spotBugs repair

--- src/main/java/io/codelex/flightplanner/FlightService.java
+++ src/main/java/io/codelex/flightplanner/FlightService.java
@@ -9,7 +9,6 @@ import org.springframework.util.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Component
 class FlightService {
